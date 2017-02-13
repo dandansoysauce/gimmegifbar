145800d8528f56ca9f7d47843559f7986c92ca17
Dani <ona.dani.a@gmail.com>
2017-02-13 17:09:53 +0800

    cleanup;

--- app/giphy-share.js
+++ app/giphy-share.js
@@ -1,5 +1,14 @@
 const electron = require('electron')
 
-export default function socialShare(social, gifLink) {
-	
+export default function socialShare(social, gifLink, gifMediaLink) {
+	const services = {
+		facebook: `http://www.facebook.com/sharer/sharer.php?u=${gifLink}`,
+		reddit: `http://www.reddit.com/submit?url=${gifLink}`,
+		twitter: `http://twitter.com/share?url=${gifLink}?tc=1`,
+		tumblr: `http://www.tumblr.com/widgets/share/tool?canonicalUrl=${gifLink}&content=${gifMediaLink}`
+	}
+
+	const service = services[social]
+
+	electron.shell.openExternal(service)
 }
